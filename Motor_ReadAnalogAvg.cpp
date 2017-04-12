2fe40dcfeec7e87d83b9fd344658d84b99b7d6df
Ethan Bloxham <bloxhaetha@myvuw.ac.nz>
2017-04-13 11:05:46 +1200

    Create Motor_ReadAnalogAvg.cpp

---
+++ Motor_ReadAnalogAvg.cpp
@@ -0,0 +1,21 @@
+# include < stdio .h > 
+# include < time .h > 
+# include " E101 .h " 
+
+int main (){ 
+init (); 
+  int adc_reading;
+  int i = 0;
+  int total = 0;
+  
+  while(i<5){
+      
+      adc_reading = read_analog(0);
+      printf("d\n", adc_reading);
+      total+= adc_reading;
+      i+ = 1;
+}
+printf(%d mean /n",  total/5);
+
+return 0;
+}
